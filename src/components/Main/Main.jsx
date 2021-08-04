6e6df8da0c66d4246f812bac81562d80e7b83110
julia_chizhik <for.yu.ch@gmail.com>
2021-08-04 15:59:11 +0300

    sum

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -3,14 +3,18 @@ import React, { Component } from 'react';
 import styles from './Main.module.scss';
 
 import Hero from './Hero';
-import Features from './Features';
+import Features1st from './Features1st';
+import Features2nd from './Features2nd';
+import Features3rd from './Features3rd';
 
 export class Main extends Component {
   render() {
     return (
       <div className={styles.container}>
         <Hero />
-        <Features />
+        <Features1st />
+        <Features2nd />
+        <Features3rd />
       </div>
     );
   }
