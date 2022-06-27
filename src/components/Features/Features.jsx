0ab8e22e3037cdae8bf2abe69ab83687cfaadda2
chizhik8 <for.yu.ch@gmail.com>
2022-06-28 02:21:53 +0300

    features data file

---
+++ src/components/Features/Features.jsx
@@ -0,0 +1,28 @@
+import React, { Component } from 'react'
+import { ReactSVG } from 'react-svg';
+
+import styles from './Features.module.scss';
+
+import FeatureItem from '../FeatureItem/FeatureItem';
+
+import rainleft from '../../assets/images/rainleft.svg';
+import rainright from '../../assets/images/rainright.svg';
+import features from '../../assets/dataFeatures';
+
+export class Features extends Component {
+  render() {
+        return (
+        <section className={styles.container}>
+          <ReactSVG src={rainleft} />
+          <div className={styles.content}>
+            <h2 className={styles.title}>Your choice</h2>
+            <p className={styles.text}>There are many reasons to get down and start to get depressed about your situation.</p>
+            <ul className={styles.list}>{features.map(item => <FeatureItem key={item.id} icon={item.icon} title={item.title} text={item.text} />)}</ul>
+          </div>
+          <ReactSVG src={rainright} />
+        </section>
+        )
+    }
+}
+
+export default Features;
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 import styles from './Main.module.scss';
 
 import Hero from '../Hero/Hero';
-import Features1st from './Features1st';
+import Features from '../Features/Features';
 import Features2nd from './Features2nd';
 import Features3rd from './Features3rd';
 import Features4th from './Features4th';
@@ -15,7 +15,7 @@ export class Main extends Component {
     return (
       <div className={styles.container}>
         <Hero />
-        <Features1st />
+        <Features />
         <Features2nd />
         <Features3rd />
         <Features4th />
