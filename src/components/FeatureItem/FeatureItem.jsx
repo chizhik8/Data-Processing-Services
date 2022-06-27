c7f0e7e58dfde8c30df5bcfbecfac00c4bcdff07
chizhik8 <for.yu.ch@gmail.com>
2022-06-28 02:21:53 +0300

    features data file

---
+++ src/components/FeatureItem/FeatureItem.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { ReactSVG } from 'react-svg';
+
+import styles from './FeatureItem.module.scss';
+
+
+function FeatureItem({icon, title, text}) {
+  return (
+    <li className={styles.container} >
+        <ReactSVG src={icon} />
+        <div className={styles.content} >
+            <h3 className={styles.title}>{title}</h3>
+            <p className={styles.text}>{text}</p>
+        </div>
+    </li>
+  )
+}
+
+export default FeatureItem;
