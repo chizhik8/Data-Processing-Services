231bce421a3e1af8feb421a8fc6f41023f68b195
chizhik8 <for.yu.ch@gmail.com>
2022-06-29 13:58:33 +0300

    added general data file

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 This project was bootstrapped with
 [Create React App](https://github.com/facebook/create-react-app).
 
-Design: 
+Based on design: 
 [https://www.figma.com/file/dKoUnl5kwWh74ipofDPixg/Landing-Page-Concept-For-Data-Processing-Company-(Copy)?node-id=1%3A2]
---
+++ src/assets/dataMain.js
@@ -0,0 +1,30 @@
+import icon1 from '../assets/icons/icon1.svg';
+import icon2 from '../assets/icons/icon2.svg';
+import icon3 from '../assets/icons/icon3.svg';
+import icon4 from '../assets/icons/icon4.svg';
+import icon5 from '../assets/icons/icon5.svg';
+import icon6 from '../assets/icons/icon6.svg';
+import trafficImg from '../assets/images/imgTraffic.svg';
+
+const main = {
+    hero: {
+        title: 'Many reasons to get up and start to get back in the business.',
+        text: 'The harder you work for something, the greater you’ll feel when you achieve it.',
+    },
+    features: [
+        {id:'icon1', icon: icon1, title:'Ecstatic elegance', text:'Article nor prepare chicken you him now. Shy merits say advice ten before lovers innate add.'},
+        {id:'icon2', icon: icon2, title:'Folly words widow', text:'Effect if in up no depend seemed. Ecstatic elegance gay but disposed. We me rent been part what.'},
+        {id:'icon3', icon: icon3, title:'Possible procured trifling', text:'We me rent been part what. An concluded sportsman offending so provision mr education.'},
+        {id:'icon4', icon: icon4, title:'Open game', text:'Shy merits say advice ten before lovers innate add. She cordially behaviour can attempted estimable.'},
+        {id:'icon5', icon: icon5, title:'Endeavor', text:'Improve ashamed married expense bed her comfort pursuit mrs. Four time took ye your as fail lady.'},
+        {id:'icon6', icon: icon6, title:'Comfort pursuit', text:'Had denoting properly jointure you occasion directly raillery. In said to of poor full be post face snug.'},
+    ],
+    traffic: {
+        title: 'Increase your business',
+        titleLstWrd: 'traffic',
+        text:'We are committed to processing the information in order to contact you and talk about your project.',
+        img: trafficImg,
+    },
+}
+
+export default main;
